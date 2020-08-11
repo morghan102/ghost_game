8d86a08ec0c52edd70b691b9b8d4dd78f5003c41
Morghan <morghan102@gmail.com>
2020-08-11 10:53:24 -0500

    finished phase 1

--- game.rb
+++ game.rb
@@ -17,16 +17,14 @@ class GhostGame
   def play_round
     @fragment = ""
     # debugger
-    until full_word?(@fragment) #this may need to change
+    until full_word?(@fragment)
       puts "---".rjust(13)
-      puts "Player #{current_player}, it's your turn."
-      puts "The current fragment is #{@fragment}." if @fragment.length > 0
-        # could put all this elsewhere, looks messy
-      take_turn(current_player)
 
+      take_turn(current_player)
       next_player!
     end
-    # methid to count score here!
+    puts "#{@players[1]} you lose this round!" #prevplay isnt showing up
+    # method to count score here!
   end
 
   def current_player
@@ -41,59 +39,50 @@ class GhostGame
     @players.rotate!
   end
 
+  def chitchat
+    puts "Player #{current_player}, it's your turn."
+    puts "The current fragment is #{@fragment}." if @fragment.length > 0
+    puts
+    puts "Please give a letter:"
+    puts "---------------------"
+  end
 
 
   def take_turn(player)
-    puts "Please give a letter:"
-    puts "---------------------"
-    letter = gets.chomp
-    puts
-    @fragment += letter if valid_play?(letter)
-    return false if full_word?(@fragment)
+    chitchat
+    letter = nil
+    until letter 
+      letter = gets.chomp
+      puts
+      @fragment += letter if valid_play?(letter)
+      puts
+      puts
+    end
   end
-
 
 
   def valid_play?(str)
     new_frag = @fragment + str
-    return false if !ALPHA.include?(str) || str.length > 1
-      # puts "That isn't a valid guess, please keep guesses to 1 letter. You miss a turn."
-      # return false
-    @dictionary.any? { |word| word.start_with?(new_frag) } #&& word.length == new_frag.length
-      # puts "That word is invalid."
-      # puts "#{current_player}, you lose!!"
-      # return false
- 
-    # elsif full_word?(new_frag)
-    #   return false
-    # end
-    # true
+    if !ALPHA.include?(str) || str.length > 1
+      puts "That isn't a valid guess. You miss a turn."
+      return false
+    end
+    @dictionary.any? { |word| word.start_with?(new_frag) }
   end
-
-#!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# i need to restruc based on theirs :(
-
-
-
-
 
 
   def full_word?(frag)
-    if @dictionary.any? do |word| 
-      word == frag && word.length == frag.length && @dictionary.none? { |word| word.length > @fragment.length}
-    end
-      # if @dictionary.none? { |word| word.length > @fragment.length} #checking new @fragment if already a word
-    # i'll check the length
-    # if the word is longer than the length of the frag
-      puts "#{current_player}, you completed the word!" 
-      return true
-  
-    end
-    false
+    dictionary.include?(frag) && (
+      dictionary.none? do |word| 
+        word.start_with?(frag) && word.length > frag.length
+      end #dict includes frag and no words in the dict
+      # that start w frag while being longer
+    )
+    #   puts "#{current_player}, you completed the word!" 
+
   end
 
 end
-# fix too so sand doesnt stop bc sandwich! not sure how to do
   #wd be nice to to correlate num of times player has gone and 
 # that frag's position in words of the dictionary
   #make output look better, it's confusng
